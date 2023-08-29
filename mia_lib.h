429e2391fbb352ef2da043f5ca2fcb1fc9692f8c
gabricampaa <135340970+gabricampaa@users.noreply.github.com>
2023-08-29 12:40:26 +0200

    Update mia_lib.h

--- mia_lib.h
+++ mia_lib.h
@@ -1,6 +1,9 @@
 #include <iostream>
 using namespace std;
 
+//scrivi is literally 'write' in italian. It's just a fancy way to print a line
+
+
 string scrivi(string input){
 
     cout<<input<<endl;
@@ -8,6 +11,8 @@ string scrivi(string input){
     
 }
 
+
+//a capo it's italian for 'starting a new line'
 
 void aCapo(){
     cout<<endl;
